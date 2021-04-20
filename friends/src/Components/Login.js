14a6b47d7a90a7c3f549e2c496924de9b916932f
abe-one <77995299+abe-one@users.noreply.github.com>
2021-04-20 15:57:25 -0500

    Post credentials

--- friends/src/Components/Login.js
+++ friends/src/Components/Login.js
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import axios from "axios";
 
 const Login = () => {
   const initialCredentials = {
@@ -18,6 +19,15 @@ const Login = () => {
   const login = (e) => {
     e.preventDefault();
     // POST credentials
+    axios
+      .post("http://localhost:5000/api/login", credentials)
+      .then((res) => {
+        console.log(res.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      })
+      .finally(() => {});
   };
 
   return (
@@ -28,9 +38,20 @@ const Login = () => {
             type="text"
             name="username"
             value={credentials.username}
+            onChange={handleChange}
             placeholder="Username"
           />
         </label>
+        <label htmlFor="password">
+          <input
+            type="password"
+            name="password"
+            value={credentials.password}
+            onChange={handleChange}
+            placeholder="Password"
+          />
+        </label>
+        <button type="submit">Login</button>
       </form>
     </div>
   );

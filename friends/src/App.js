588c052558b7f608e1a58b271244666aa6c1af8a
abe-one <77995299+abe-one@users.noreply.github.com>
2021-04-20 15:07:19 -0500

    Setup

---
+++ friends/src/App.js
@@ -0,0 +1,49 @@
+import logo from "./logo.svg";
+import "./App.css";
+import { BrowserRouter as Router, Route, Link, Switch } from "react-router-dom";
+
+/*
+Wrap everything in Router
+Create Links
+Route links to components
+  according to switch
+    Login
+    FriendList
+Build Components
+  Login
+  FriendList
+    FriendForm
+      'Add' Functionality
+Write axios
+  login
+  auth
+    get login
+    post friend
+Private Route Protected pages 
+
+  
+ */
+
+function App() {
+  const logout = () => {
+    // req to api re logout
+    // remove local token
+  };
+
+  return (
+    <Router>
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <Link to="/login">Login</Link>
+          <Link to="/login" onClick={logout}>
+            Logout
+          </Link>
+          <Link to="/friendslist">Tip Top Secret Friends List</Link>
+        </header>
+      </div>
+    </Router>
+  );
+}
+
+export default App;

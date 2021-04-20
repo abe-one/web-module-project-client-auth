4ce306a99900d9735a19a1d1ecb860d318745b42
abe-one <77995299+abe-one@users.noreply.github.com>
2021-04-20 15:48:00 -0500

    Begin Login

--- friends/src/Components/Login.js
+++ friends/src/Components/Login.js
@@ -1,7 +1,39 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Login = () => {
-  return <div></div>;
+  const initialCredentials = {
+    username: "",
+    password: "",
+  };
+
+  const [credentials, setCredentials] = useState(initialCredentials);
+
+  const handleChange = (e) => {
+    setCredentials({
+      ...credentials,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const login = (e) => {
+    e.preventDefault();
+    // POST credentials
+  };
+
+  return (
+    <div>
+      <form onSubmit={login}>
+        <label htmlFor="username">
+          <input
+            type="text"
+            name="username"
+            value={credentials.username}
+            placeholder="Username"
+          />
+        </label>
+      </form>
+    </div>
+  );
 };
 
 export default Login;
